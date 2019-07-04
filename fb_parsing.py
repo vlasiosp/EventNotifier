dd47c8aeaadb6ecc67a202c00341aab4df50b07e
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-07-04 17:22:19 +0300

    Delete functions to put them in a separate functions file

--- fb_parsing.py
+++ fb_parsing.py
@@ -5,7 +5,7 @@ from  selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 import random
-
+from functions import *
 
 options = Options()
 options.headless = False
@@ -20,20 +20,6 @@ options.add_experimental_option("prefs", {
 
 
 browser = webdriver.Chrome(options=options, executable_path=r'webdrivers/chromedriver.exe')
-
-
-# Login function
-def fb_login(email, passw):
-    browser.get('https://www.facebook.com')
-    browser.find_element_by_id("email").send_keys(email)
-    browser.find_element_by_id("pass").send_keys(passw)
-    browser.find_element_by_id('loginbutton').click()
-
-
-# Search function
-def fb_search(text):
-    browser.find_element_by_class_name('_1frb').send_keys(text)
-    browser.find_element_by_css_selector("button[data-testid='facebar_search_button']").click()
 
 
 fb_login('teipir@gmail.com', '19eptalofou76')
