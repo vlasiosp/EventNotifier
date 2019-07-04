03d6074534d66c27237f54eefbcb97bc636314de
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-07-04 12:10:45 +0300

    Improve error handler Replace time interval with a random number 1-4 secs

--- fb_parsing.py
+++ fb_parsing.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from  selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+import random
 
 
 options = Options()
@@ -19,31 +22,32 @@ options.add_experimental_option("prefs", {
 })
 
 browser = webdriver.Chrome(options=options, executable_path=r"E:\GitHub\EventNotifier\webdrivers\chromedriver.exe")
-# go to the google home page
 
 
 
+# Login function
 def fb_login(email, passw):
     browser.get('https://www.facebook.com')
     browser.find_element_by_id("email").send_keys(email)
     browser.find_element_by_id("pass").send_keys(passw)
     browser.find_element_by_id('loginbutton').click()
 
+#Search function
 def fb_search(text):
     browser.find_element_by_class_name('_1frb').send_keys(text)
     browser.find_element_by_css_selector("button[data-testid='facebar_search_button']").click()
 
 
-fb_login("teipir@gmail.com","19eptalofou76")
+fb_login('teipir@gmail.com','19eptalofou76')
 
-fb_search("Champions")
+fb_search('Champions')
 
 
 
 
 
 try:
-    element = WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.ID, 'Id_Of_Element')))
+    element = WebDriverWait(browser, random.randrange(1,4,1)).until(EC.presence_of_element_located((By.ID, 'facebook')))
 except TimeoutException:
     print("Time out!")
 
