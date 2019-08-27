cb2f97164f088fe809ce5e18460891144baec87e
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-08-27 16:50:29 +0300

    Create a user function which prints friends and followers

--- keys_urls.py
+++ keys_urls.py
@@ -8,7 +8,7 @@ ck = 'c532YnnqRwaQBMDd1vT7VWtwX'
 cs = 'ypxfs5PTyZxDavfn9kGFHAE6pN4CC1IiyrmJQLK8LJHx49YOOA'
 
 # Access_token
-ac = '3405775702-XlxH1XnIdoPXykWqwvX8ODjaAmNOxa5LIprprRJ'
+at = '3405775702-XlxH1XnIdoPXykWqwvX8ODjaAmNOxa5LIprprRJ'
 
 # Access_token_secret
 ats = '8IKa3Fb36VHPVrtWV0vGYliPXYnhZcoqn9YaMkCAqZecY'
--- tweet_app.py
+++ tweet_app.py
@@ -1,11 +1,26 @@
 import keys_urls
 import tweepy
 
+
+def user_api():
+    user = api.get_user('KostasVaxevanis')
+    print(user.screen_name)
+    print("The number of followers of user",user.screen_name, "are",user.followers_count)
+    print("His friends are: ")
+    for friend in user.friends():
+        print(friend.screen_name)
+
+
 auth = tweepy.OAuthHandler(keys_urls.ck, keys_urls.cs)
-auth.set_access_token(keys_urls.ac, keys_urls.ats)
+auth.set_access_token(keys_urls.at, keys_urls.ats)
 
 api = tweepy.API(auth)
+
+# status = "Testing!"
+# api.update_status(status=status)
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+user_api()
