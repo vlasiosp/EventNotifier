14b92e1c5f5f16108e529f60eb5b245a5cc40433
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-09-04 11:30:11 +0300

    construct a basic stream listener class

--- tweet_app.py
+++ tweet_app.py
@@ -1,32 +1,42 @@
 import keys_urls
 import tweepy
 
+chania_loc="35.510457, 24.022692"
+
 # ---Functions ---
-def user_api():
+def user_details():
     user = api.get_user('KostasVaxevanis')
     print(user.screen_name)
-    print("The number of followers of user",user.screen_name, "are",user.followers_count)
+    print("The number of followers of user", user.screen_name, "are",user.followers_count)
     print("His friends are: ")
     for friend in user.friends():
         print(friend.screen_name)
 
 
+#override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
 def search_hashtag():
-    for tweet in tweepy.Cursor(api.search, q="#Chania", count=100,
+    for tweet in tweepy.Cursor(api.search, q="#Chania",count=100,
                                lang="en",
-                               since="2017-04-03").items():
-        print(tweet.created_at, tweet.text)
+                               since="2017-04-03").items(1000):
+        print(tweet.created_at, tweet.user.screen_name, tweet.text, tweet.place)
+
+
+# --- User API Authentication ---
 
 auth = tweepy.OAuthHandler(keys_urls.ck, keys_urls.cs)
 auth.set_access_token(keys_urls.at, keys_urls.ats)
-
 api = tweepy.API(auth)
 
-# status = "Testing!"
-# api.update_status(status=status)
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
 
-user_api()
+
+#search_hashtag()
+MyStreamListener()
