3d2d50f34f504e769bbfd8e55e1b6384e060ac31
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-09-11 21:16:16 +0300

    Crete authenticator file with api credentials

---
+++ authenticator.py
@@ -0,0 +1,20 @@
+class authentication:
+
+	def __init__(self):
+		# The consumer key and secret will be generated for you after
+		self.consumer_key ="c532YnnqRwaQBMDd1vT7VWtwX"
+		self.consumer_secret="ypxfs5PTyZxDavfn9kGFHAE6pN4CC1IiyrmJQLK8LJHx49YOOA"
+
+		self.access_token="3405775702-XlxH1XnIdoPXykWqwvX8ODjaAmNOxa5LIprprRJ"
+		self.access_token_secret="8IKa3Fb36VHPVrtWV0vGYliPXYnhZcoqn9YaMkCAqZecY"
+
+	def getconsumer_key(self):
+		return self.consumer_key
+
+	def getconsumer_secret(self):
+		return self.consumer_secret
+	def getaccess_token(self):
+		return self.access_token
+	def getaccess_token_secret(self):
+		return self.access_token_secret
+
