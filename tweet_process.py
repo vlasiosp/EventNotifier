bc3b25dab55158dddf51ca0e0b4ddb3082aa0dd6
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-11-04 12:59:43 +0200

    Create file for tweet processing

---
+++ tweet_process.py
@@ -0,0 +1,64 @@
+import nltk
+from nltk import word_tokenize, wordpunct_tokenize, sent_tokenize
+import MySQLdb
+from mysql_conn import db_connect
+
+
+
+
+
+
+
+
+def word_split():
+    con=db_connect()
+    cursor=con.cursor()
+
+    #Select all users' screennames from db as tuple
+
+    user_select_query = "SELECT ScreenName from USERS"
+    cursor.execute(user_select_query)
+    user_select = cursor.fetchall()
+
+# make a list and remove some special characters
+    user_select = list(sum(user_select, ()))
+
+
+
+
+
+
+
+    print (user_select)
+
+    for user in user_select:
+
+        print(user)
+        tweet_select_query = """SELECT TweetText FROM `TWEETS` where ScreenName = '&s'"""
+        cursor.execute(tweet_select_query, user)
+        tweet_select = cursor.fetchall()
+        print(tweet_select)
+
+
+
+
+
+
+
+
+
+
+        '''for tweet in tweet_select:
+            tokened[i] = word_tokenize(tweet)
+            print(tokened[i])
+            i+=1'''
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    word_split()
+
