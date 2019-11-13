34c395caa9f3bfad53d00c9c1b7c5e0b13ee09e2
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-11-13 11:40:48 +0200

    Tokenize all user's tweets

--- tweet_process.py
+++ tweet_process.py
@@ -1,28 +1,25 @@
 import nltk
+import re
 from nltk import word_tokenize, wordpunct_tokenize, sent_tokenize
 import MySQLdb
 from mysql_conn import db_connect
 
+def word_process(tweet):
+    tweet_tokenize = word_tokenize((tweet))
+    print("Tweet is", tweet_tokenize)
 
-
-
-
-
-
-
-def word_split():
+def tweet_selector():
     con=db_connect()
     cursor=con.cursor()
 
-    #Select all users' screennames from db as tuple
+# Select all users' screennames from db as tuple
 
     user_select_query = "SELECT ScreenName from USERS"
     cursor.execute(user_select_query)
     user_select = cursor.fetchall()
     print(user_select)
 
-
-    # make a list and remove some special characters
+# make a list and remove some special characters
     user_select = list(sum(user_select, ()))
 
     print(user_select)
@@ -30,9 +27,13 @@ def word_split():
     for user in user_select:
 
         print(user)
-        query = cursor.execute("""SELECT TweetText FROM `TWEETS` where ScreenName = %s""", (user,))
+        cursor.execute("""SELECT TweetText FROM `TWEETS` where ScreenName = %s""", (user,))
         q_f = cursor.fetchall()
-        print(q_f)
+        q_f = list(sum(q_f , ()))
+        for tweet in q_f:
+            word_process(tweet)
+
+
 
 
 
@@ -56,5 +57,5 @@ def word_split():
 
 if __name__ == '__main__':
 
-    word_split()
+    tweet_selector()
 
