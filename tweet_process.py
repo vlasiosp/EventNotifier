ff3588f1af3fb571d9b67feeb25f91da8472aca5
Vlasios - Alkis Papavasieliou <45150475+vlasiosp@users.noreply.github.com>
2019-11-26 13:50:53 +0200

    Add a counter that counts user's tweets and resets every time the user 's tweets change'

--- tweet_process.py
+++ tweet_process.py
@@ -1,12 +1,24 @@
+from googletrans import Translator
 import nltk
-import re
 from nltk import word_tokenize, wordpunct_tokenize, sent_tokenize
-import MySQLdb
+import itertools
 from mysql_conn import db_connect
+import langdetect
+from langdetect import detect, detect_langs
 
-def word_process(tweet):
-    tweet_tokenize = word_tokenize((tweet))
-    print("Tweet is", tweet_tokenize)
+translator =  Translator()
+
+def tweet_process(tweet):
+    tw_tknz= sent_tokenize(tweet)
+    return tw_tknz
+
+def tweet_translate(tweet):
+    if tweet == translator.translate(tweet, src='el'):
+        pass
+    else:
+        tweet = translator.translate(tweet, src='auto', dest='el')
+    print("Translated Tweet:", tweet)
+
 
 def tweet_selector():
     con=db_connect()
@@ -18,41 +30,28 @@ def tweet_selector():
     cursor.execute(user_select_query)
     user_select = cursor.fetchall()
     print(user_select)
-
+    tw_list=[]
 # make a list and remove some special characters
     user_select = list(sum(user_select, ()))
 
     print(user_select)
-
+    counter=0
     for user in user_select:
 
         print(user)
         cursor.execute("""SELECT TweetText FROM `TWEETS` where ScreenName = %s""", (user,))
         q_f = cursor.fetchall()
         q_f = list(sum(q_f , ()))
-        for tweet in q_f:
-            word_process(tweet)
-
-
-
-
-
-
-
-
-
-
-
-
-
-    '''for tweet in tweet_select:
-            tokened[i] = word_tokenize(tweet)
-            print(tokened[i])
-        i+=1'''
-
-
-
-
+        counter=0
+        for tweet in q_f: # Για κάθε tweet ενός χρήστη
+            tw_pr =  tweet_process(tweet)
+            counter+=1
+            if counter ==q_f[-1]:
+                counter=0
+            print(tw_pr, counter)
+            # print ("/n",tw_pr)
+            tw_list.append(tw_pr)
+            # print(tw_list)
 
 
 if __name__ == '__main__':
